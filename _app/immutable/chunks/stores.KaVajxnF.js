17f783717a5ab540277700a0001e87755f4c15e7
Anton Hackl <antonhackl0@gmail.com>
2024-11-07 19:05:48 +0100

    Updates

--- _app/immutable/chunks/stores.KaVajxnF.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./entry.0GuV2PcP.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
