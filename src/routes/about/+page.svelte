1fa3e8913f9446b48f52bc41020ba4dbded851db
Anton Hackl <antonhackl0@gmail.com>
2024-12-01 16:18:28 +0100

    Change wording

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,11 +10,11 @@
       <h1>About</h1>
     </div>
     <p>
-      Almost a year ago we started with the organization and development of the HPI Poker Bot Competition as a side project.
+      Almost a year ago we started with the organization and development of a Poker Bot Competition as a side project.
     </p>
     <p>
       Inspired by the <a href="https://pokerbots.org/">MIT Pokerbots</a> event we wanted to create a similar competition at HPI...
-      This year we are proud to present the first edition of the HPI Poker Bot Competition!
+      This year we are proud to present the first edition of HPI Showdown's Poker Bot Competition!
     </p>
   </div>
   <About />
